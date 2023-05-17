8a2065ae2b8547a4cd616a6af1b342898c416b8b
Amari Pascoe <elliotpascoe04@gmail.com>
2023-05-17 17:02:26 -0400

    Completed.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ let rows = 25;
 let cols = 25;
 let canvas;
 let context;
+
+//food variables
 let foodX;
 let foodY;
 
@@ -14,6 +16,12 @@ let headY = block * 5;
 //snake vel
 let velocityX = 0;
 let velocityY = 0;
+
+//snake body
+let body = [];
+
+//gameOver 
+let gameOver = false;
 
 //canvas creation 
 window.onload = function() {
@@ -31,6 +39,9 @@ window.onload = function() {
 
 //drawing and updating  components
 function update() {
+    if (gameOver) {
+        return;
+    }
     //canvas
     context.fillStyle = 'black';
     context.fillRect(0, 0, c.height, c.width);
@@ -41,8 +52,17 @@ function update() {
 
     //if you eat the food, it spawns in a new location
     if(headX == foodX && headY == foodY) {
+        body.push([foodX, foodY]);
         placeFood();
     }
+
+    //extend body
+    for (let i = body.length - 1; i >= 0; i-- ) {
+        body[i] = body[i + 1];
+    }
+     if (body.length) {
+        body[0] = [headX, headY];
+     }
 
     //snake headd 
     context.fillStyle='green';
@@ -50,10 +70,23 @@ function update() {
     headX += velocityX * block;
     headY += velocityY * block;
     context.fillRect(headX, headY, block, block);
-
+    for (let i = 0; i < body.length; i++) {
+        context.fillRect(body[i][0], body[i][1], block, block);
+    }
+    //YOU LOSE (game over) scenarios...
+    if (headX < 0 || headX > cols*block || headY < 0 || headY > rows * block) {
+        gameOver = true;
+        alert("YOU LOSE");
+    }
+    for (let i = 0; i < body.length; i++) {
+        if (headX == body[i][0] && headY == body[i][1]){
+            gameOver = true;
+            alert("YOU LOSE");
+        }
+    }
 }
 
-//movement
+//movement with arrow keys, cannot move in the oppsite direction of current one
 function moveAround(e) {
     if (e.code == 'ArrowUp' && velocityY != 1) {
         velocityX = 0;
