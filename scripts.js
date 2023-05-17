cf8fad7e1dbe35b9c2461c80752a0e5de527f12e
Amari Pascoe <elliotpascoe04@gmail.com>
2023-05-17 15:13:52 -0400

    basic canvas setup

--- scripts.js
+++ scripts.js
@@ -2,6 +2,25 @@
 const block = 25;
 const rows = 20;
 const cols = 20;
-const canvas = c;
+let canvas;
+let context;
+
+//canvas creation 
+window.onload = function() {
+    c = document.getElementById("canvas");
+    c.height = rows * block;
+    c.width = cols * block;
+    context = c.getContext("2d");
+
+    update();
+
+}
+
+//drawing and updating the canvas
+function update() {
+    context.fillStyle = 'gray';
+    context.fillRect(0, 0, c.height, c.width);
+}
+
 
 
